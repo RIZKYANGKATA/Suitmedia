5e776bd41d5048a5da7dfc3db727bf841498e4f7
RIZKYANGKATA <rizmadoris@gmail.com>
2023-12-24 01:22:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="styleIndex.css">
+    </head>
+
+    <!-- Navbar -->
+<header class="nav">
+    <div id="logo-sm">
+        <img src="sm2.png" alt="Logo">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="">Work</a></li>
+            <li><a href="#card">About</a></li>
+            <li><a href="">Service</a></li>
+            <li><a href="">Ideas</a></li>
+            <li><a href="">Careers</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        </div>
+    </nav>
+</header>
+<!-- Akhir Navbar -->
+
+<body class="align-items-start d-flex flex-column">
+
+<img class="gambar" src="bg.jpg" alt="">
+
+    <!-- Card 1-->
+    <div id="card">
+    <div class="container-fluid d-flex justify-content-center">
+    
+        
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quod voluptates at fugiat eligendi nulla repellendus, labore perferendis facilis itaque corporis modi eius natus commodi! Distinctio rem beatae nam non?</b></p>
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias sed ipsam quia praesentium natus, consequatur voluptas perspiciatis molestias deleniti. Architecto maiores, aliquam laboriosam corrupti quaerat placeat ea perferendis necessitatibus ducimus!</b></p>
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat esse magni iusto dolorem saepe ea sunt corporis tenetur? Nihil ut enim cum iusto. Voluptas ipsum at sint nisi, dolorum doloremque.</b></p>
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut dolor ipsam dolorum qui assumenda harum optio corrupti distinctio tempore in quae ratione, facere cupiditate. Magni, corporis vel! Sit, ab saepe?</b></p>
+    </div>
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+
+    <div class="container-fluid d-flex justify-content-center">  
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, exercitationem sunt repudiandae nulla non molestiae dolor corporis unde tenetur nihil. Optio non, odio voluptatibus id eum voluptatum nihil pariatur eligendi.</b></p>
+        
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta reprehenderit cum officia doloremque nulla praesentium sunt, rem pariatur iusto temporibus, eveniet totam fugiat quibusdam consequatur expedita fugit vel quisquam tenetur.</b></p>
+        
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque, dolorum incidunt aut tempore deserunt excepturi ipsam recusandae quod temporibus repudiandae. Commodi repellat magni alias architecto, veniam quod sequi ipsum.</b></p>
+        
+    </div>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <div class="card" style="width: 18rem; border-radius: 10%;">
+    <div><img src="card1.png" class="card-img-top img-fluid mx-auto" alt="..."></div>
+    <div class="card-body">
+        <p class="card-title">Lorem</p>
+        <p class="card-text"><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aliquam nesciunt tenetur nostrum eligendi, nemo provident quod aspernatur architecto quos, corporis ipsam expedita libero inventore consectetur unde deserunt nisi non.</b></p>
+        
+    </div>
+    </div>
+    </div>
+
+
+    </div>
+</body>
+
+</html>
